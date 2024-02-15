3097198a5777e51e41df4f8779c8a435e412b570
Ohsori001106 <surnameMin@Gmail.com>
2024-02-15 12:29:56 +0900

    미니맵 + 폭탄

---
+++ Assets/02.Scripts/Weapon/Bomb.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject BoomPrefab;
+    private void OnCollisionEnter(Collision collision)
+    {
+      
+        if (collision.collider.tag != "Player")
+        {
+            Instantiate(BoomPrefab, transform.position, transform.rotation);
+
+           /* GameObject effect = Instantiate(BoomPrefab);
+            effect.transform.position = this.gameObject.transform.position;*/
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
